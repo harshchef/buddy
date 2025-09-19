60021ff59fe319eda90b6fead9c6e55659b482cd
Aditya Kumar <adityakumar0718@gmail.com>
2025-09-19 13:02:53 +0530

    first commit to update the uri

---
+++ src/main/java/com/aiknowledge/buddy/AiKnowledgeTransferBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.aiknowledge.buddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiKnowledgeTransferBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiKnowledgeTransferBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aiknowledge/buddy/AiKnowledgeTransferBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiknowledge.buddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiKnowledgeTransferBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
